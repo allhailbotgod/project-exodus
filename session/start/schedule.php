37e598c8d62262d63ce230cbec83727c64cfd582
allhailbotgod <codenameopvirus@gmail.com>
2025-12-23 18:07:12 +0100

    Still rolling

---
+++ session/start/schedule.php
@@ -0,0 +1,22 @@
+<!-- CODE BY BOTGODJAY -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HFYV Shedule</title>
+  <link rel="stylesheet" href="../../styles/schedule.css">
+</head>
+<body>
+  <img src="../../images/logo.png" alt="HFYV Logo">
+  <h2>CHECK SCHEDULE</h2>
+  <button class="ref">REFECTORY</button>
+  <button class="mart">SUPERMART</button>
+  <button class="bus">BUS MOVEMENT</button>
+
+  <footer>
+    <p>&copy; HOLY FAMILY YOUTH VILLAGE</p>
+  </footer>
+</body>
+</html>
